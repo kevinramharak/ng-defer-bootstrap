6d7a5881baf30d3bb2d7f8b5badebcd1887ad3ff
Kevin Ramharak <kevin@ramharak.nl>
2019-10-03 13:41:05 +0200

    refactor to actually implement allSettled

--- src/ng-defer-bootstrap.ts
+++ src/ng-defer-bootstrap.ts
@@ -8,7 +8,6 @@
     // use the angular module to retrieve the $q and $log services
     const ng = angular.injector(['ng']);
     const $q = ng.get('$q');
-    const $log = ng.get('$log');
 
     function setDeferFlag() {
         if (window.name.indexOf(NG_BOOTSTRAP_DEFER) !== 0) {
@@ -25,9 +24,24 @@
         }
     }
 
+    type AllSettledFulfilledReturnvalue<T = any>  = { status: 'fulfilled', value: T };
+    type AllSettledRejectedReturnvalue<R = any>  = { status: 'rejected', reason: R };
+
+    /**
+     * see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise/allSettled
+     * and: https://tc39.es/proposal-promise-allSettled/#sec-promise.allsettled
+     */
+    function allSettled<T = any, R = any>(promises: PromiseLike<any>[]): PromiseLike<(AllSettledFulfilledReturnvalue<T> | AllSettledRejectedReturnvalue<R>)[]> {
+        return $q.all(promises.map((promise => {
+            return $q((resolve: (status: AllSettledFulfilledReturnvalue<T> | AllSettledRejectedReturnvalue<R>) => any) => {
+                promise.then((value) => resolve({ status: 'fulfilled', value }), (reason) => resolve({ status: 'rejected', reason }));
+            });
+        })));
+    }
+
     function resumeDeferredBootstrap() {
         const promises = callbacks.map(callback => $q.when(callback()));
-        $q.all(promises)
+        allSettled<string[] | void>(promises)
             .then((values) => {
                 values.forEach(value => {
                     if (Array.isArray(value)) {
@@ -37,8 +51,7 @@
                 // We know resume bootstrap is defined because this hook gets called after the `angular.resumeBootstrap` property is set
                 // see: https://github.com/angular/angular.js/blob/master/src/Angular.js#L1846
                 angular.resumeBootstrap!(modules);
-            })
-            .catch((reason: string) => $log.error(`ngDeferBootstrap: could not resume bootstrapping process because a promise got rejected with the reason: '${reason}'`));
+            });
     }
 
     // set methods on `IAngularStatic` singleton
